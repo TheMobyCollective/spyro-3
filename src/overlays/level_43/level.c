4dc0ae1ce1819bc3f67d48c0f47819461cf7ce26
Ellis Caird <ellisc15060@gmail.com>
2026-02-17 19:06:45 +0000

    added func_level_43_8008DDE4.s

--- src/overlays/level_43/level.c
+++ src/overlays/level_43/level.c
@@ -12,8 +12,21 @@ extern int D_8006E044;
 extern unsigned int D_8006E048;
 extern Spyro spyro;
 extern void func_8004F178(Vector3D*, Vector3D*); // fSetVector
-                                                 //
-void func_level_43_8008B8B4(Moby* arg0, int arg1) {
+extern void func_800293C4(unsigned short, unsigned short, int, int, int);           
+extern void func_80029674(void*, int*, int*);                 
+extern void func_80029708(void*, int*, int*, int*);
+
+typedef struct {
+    short unk0;
+    short unk2;
+    unsigned char unk4 [0x22]; //pad?
+    short unk26;
+    unsigned char unk28 [0x17]; //pad?
+    unsigned char unk3F;
+    int unk40;
+} ARG_8008DDE4;
+
+extern void func_level_43_8008B8B4(Moby* arg0, int arg1) {
     spyro.unk20a = 1;
     spyro.unk20b = arg1;
     spyro.critterMobyPtr = arg0;
@@ -72,4 +85,31 @@ INCLUDE_ASM("asm/nonmatchings/overlays/level_43/level", func_level_43_8008D96C);
 
 INCLUDE_ASM("asm/nonmatchings/overlays/level_43/level", func_level_43_8008DB04);
 
-INCLUDE_ASM("asm/nonmatchings/overlays/level_43/level", func_level_43_8008DDE4);
+
+
+
+
+
+void func_level_43_8008DDE4(ARG_8008DDE4* arg0) {
+    int sp18;
+    int sp1C;
+    int sp20;
+    int sp24;
+    int sp28;
+    unsigned char temp_v0;
+
+    sp18 = (int) arg0->unk0;
+    sp1C = (int) arg0->unk2;
+    temp_v0 = arg0->unk3F;
+    if (temp_v0 != 0) {
+        sp20 = (int) (temp_v0 + 0xA) >> 1;
+    } else {
+        sp20 = 0;
+    }
+    func_80029674(arg0, &sp18, &sp1C);
+    sp24 = sp18;
+    sp28 = sp1C;
+    func_80029708(arg0, &sp20, &sp24, &sp28);
+    func_800293C4(arg0->unk40, arg0->unk26, sp18, sp1C, sp20);
+}
+
