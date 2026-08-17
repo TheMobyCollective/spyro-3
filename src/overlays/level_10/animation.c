a6273040dd9831cd862aa6a69b97af0bb6a4b1d8
Hwd405 <108627843+Hwd405@users.noreply.github.com>
2026-08-17 17:51:46 +0100

    Start matching more overlay functions

--- src/overlays/level_10/animation.c
+++ src/overlays/level_10/animation.c
@@ -4,8 +4,19 @@ INCLUDE_ASM("asm/nonmatchings/overlays/level_10/animation", func_level_10_800881
 
 INCLUDE_ASM("asm/nonmatchings/overlays/level_10/animation", func_level_10_800897A4);
 
+/**
+ * func_level_10_80089B9C() - ???() - MATCHING
+ * Ready to add. Probably a Spyro animation sounds related one. Probably try making an animation.inc.h.
+ * https://decomp.me/scratch/HatAh
+ */
 INCLUDE_ASM("asm/nonmatchings/overlays/level_10/animation", func_level_10_80089B9C);
 
+/**
+ * func_level_10_80089CA4() - ???() - MATCHING
+ * Ready to add. Check comments in scratch first, will probably try adding this using an animation.inc.h instead.
+ * Likely has matches across other levels.
+ * https://decomp.me/scratch/4KNXz
+ */
 INCLUDE_ASM("asm/nonmatchings/overlays/level_10/animation", func_level_10_80089CA4);
 
 INCLUDE_ASM("asm/nonmatchings/overlays/level_10/animation", func_level_10_80089D20);
