331f2e3fca8c616b14d35fb141107bf4b01c465c
Hwd405 <108627843+Hwd405@users.noreply.github.com>
2026-08-18 15:06:19 +0100

    Update animation.c for level 12

--- src/overlays/level_12/animation.c
+++ src/overlays/level_12/animation.c
@@ -4,8 +4,18 @@ INCLUDE_ASM("asm/nonmatchings/overlays/level_12/animation", func_level_12_800886
 
 INCLUDE_ASM("asm/nonmatchings/overlays/level_12/animation", func_level_12_80089B6C);
 
+/**
+ * func_level_12_80089FE4() - ???() - MATCHING
+ * Ready to add. Currently appropriate for introducing as an animation.inc.h.
+ * https://decomp.me/scratch/CMQIW
+ */
 INCLUDE_ASM("asm/nonmatchings/overlays/level_12/animation", func_level_12_80089FE4);
 
+/**
+ * func_level_12_8008A0EC() - ???() - MATCHING
+ * Ready to add. The case order seems to be a mess here.
+ * https://decomp.me/scratch/pcTDp
+ */
 INCLUDE_ASM("asm/nonmatchings/overlays/level_12/animation", func_level_12_8008A0EC);
 
 INCLUDE_ASM("asm/nonmatchings/overlays/level_12/animation", func_level_12_8008A188);
