9a24866ce19633d2d0d51dc579cba8f27d2043f9
Hwd405 <108627843+Hwd405@users.noreply.github.com>
2026-08-19 13:32:03 +0100

    Update animation.inc.h

--- include/overlay/animation.inc.h
+++ include/overlay/animation.inc.h
@@ -18,43 +18,47 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Function 3 (Spyro and critter animation sounds)
 
-// Levels 10 to 12 confirmed in decomp.me so far, none confirmed with a build
+// Levels 10 to 15 confirmed in decomp.me so far, none confirmed with a build
 // https://decomp.me/scratch/HatAh
 // https://decomp.me/scratch/WDEnn
 // https://decomp.me/scratch/CMQIW
+// https://decomp.me/scratch/7MBYI
+// https://decomp.me/scratch/9jeXH
+// https://decomp.me/scratch/YLLhe
+
+// 15 is the most updated
 
 // The following levels will have no cases:
 // 16, 18, 28, 38, 40, 48
-// Issues:
-// * 16 is currently defined as a swimming level, this is likely not considered the case
 
-// The following levels are likely similar but probably not all identical (103 lines, likely single cases):
-// 10, 12, 14, 17, 25, 27, 35, 45, 47
+// The following levels appear to be swim only
+// 10, 14, 15, 25, 35, 45
 
-// The following levels are likely similar or identical (112 lines, likely two cases):
-// 26, 36, 46
+// The following levels appear to be burn only
+// 12, 17, 27, 47
 
-// The following levels are likely similar or identical (119 lines, likely two cases):
-// 11, 32, 37
-// 11 and 32 are both swimming / skateboarding levels and are thus candidates for perfect matches
-// 37 is probably different
+// Water and skate
+// 11, 32
 
-// 131 lines
-// 13, 22, 24, 41
+// Bentley? and water (TODO)
+// 24, 41
 
-// 136 lines
-// 21, 50
-
-// 190 lines
+// All the same (TODO)
 // 30, 43, 44
 
-// Remaining levels are unique
+// Therefore the following levels are left to do:
+// 20 21 22 23 24    26
+// 30 31    33 34    36 37
+//    41 42 43 44    46
+// 50
 
+
+
+// Does not include 16 for some reason
 #if (LEVEL_ID == 10) || (LEVEL_ID == 11) || (LEVEL_ID == 14) || (LEVEL_ID == 15) \
- || (LEVEL_ID == 16) || (LEVEL_ID == 20) || (LEVEL_ID == 21) || (LEVEL_ID == 23) \
- || (LEVEL_ID == 24) || (LEVEL_ID == 25) || (LEVEL_ID == 30) || (LEVEL_ID == 32) \
- || (LEVEL_ID == 35) || (LEVEL_ID == 41) || (LEVEL_ID == 43) || (LEVEL_ID == 44) \
- || (LEVEL_ID == 45)
+ || (LEVEL_ID == 20) || (LEVEL_ID == 21) || (LEVEL_ID == 23) || (LEVEL_ID == 24) \
+ || (LEVEL_ID == 25) || (LEVEL_ID == 30) || (LEVEL_ID == 32) || (LEVEL_ID == 35) \
+ || (LEVEL_ID == 41) || (LEVEL_ID == 43) || (LEVEL_ID == 44) || (LEVEL_ID == 45)
     #define IS_SWIMMING_LEVEL
 #endif
 
@@ -69,13 +73,16 @@
     #define IS_FIRE_LAVA_LEVEL
 #endif
 
-// WIP - taken from decomp.me on August 18th 2026
+#if (LEVEL_ID == 13) || (LEVEL_ID == 22) || (LEVEL_ID == 26) || (LEVEL_ID == 34)
+    #define IS_SGT_BYRD_LEVEL
+#endif
+
+// WIP - taken from decomp.me on August 19th 2026
 void NAME_OVERLAY_FUNCTION(UnnamedOverlayFunction_Animation3) (void) {
     int animationId;
     int animationFrame;
 
     if (spyro.unk20a != 0) {
-        // convert critter animation ID to Spyro equivalent? At least relative to swim
         animationId = spyro.critterMobyPtr->animationState.id;
         switch (spyro.critterMode) {
         case CRITTER_SHEILA:
@@ -111,8 +118,7 @@ void NAME_OVERLAY_FUNCTION(UnnamedOverlayFunction_Animation3) (void) {
         animationId = spyro.bodyAnimation.id;
         animationFrame = spyro.bodyAnimation.frame;
     }
-
-    // For some levels this will be empty which should result in the whole function being empty
+    
     switch (animationId) {
 
     #ifdef IS_SKATEBOARDING_LEVEL
@@ -141,7 +147,19 @@ void NAME_OVERLAY_FUNCTION(UnnamedOverlayFunction_Animation3) (void) {
         }
         break;
     #endif
-	
+
+    #ifdef IS_SGT_BYRD_LEVEL
+    case 0x65:
+        if (animationFrame == 2 || animationFrame == 7) {
+            animationId = func_8003BABC(spyro.critterMobyPtr, 0, 0);
+            spyro.unk22[6] = animationFrame;
+            if (animationId >= 0 && animationFrame == 7) {
+                func_8003C0B0(animationId, 0x1200);
+            }
+        }
+        break;
+    #endif
+        
     }
 }
 
