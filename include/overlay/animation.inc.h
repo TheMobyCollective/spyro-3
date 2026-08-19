b8b6cfd644279004d3369ddc2ddcf17ca4c45023
Hwd405 <108627843+Hwd405@users.noreply.github.com>
2026-08-19 22:04:43 +0100

    Update animation.inc.h

--- include/overlay/animation.inc.h
+++ include/overlay/animation.inc.h
@@ -18,48 +18,33 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Function 3 (Spyro and critter animation sounds)
 
-// Levels 10 to 15 confirmed in decomp.me so far, none confirmed with a build
-// https://decomp.me/scratch/HatAh
-// https://decomp.me/scratch/WDEnn
-// https://decomp.me/scratch/CMQIW
-// https://decomp.me/scratch/7MBYI
-// https://decomp.me/scratch/9jeXH
-// https://decomp.me/scratch/YLLhe
+// Some cases excluded due to repetition
+// 10 https://decomp.me/scratch/HatAh
+// 11 https://decomp.me/scratch/WDEnn
+// 12 https://decomp.me/scratch/CMQIW
+// 13 https://decomp.me/scratch/7MBYI
+// 14 https://decomp.me/scratch/9jeXH
+// 15 https://decomp.me/scratch/YLLhe
+// 20 https://decomp.me/scratch/T3LWL
+// 21 https://decomp.me/scratch/3l0K5
+// 22 https://decomp.me/scratch/7dyJm
+// 23 https://decomp.me/scratch/CVnqu
+// 24 https://decomp.me/scratch/L1Ee8
+// 26 https://decomp.me/scratch/Qb1Vq
+// 30 https://decomp.me/scratch/hozW3
+// 31 https://decomp.me/scratch/sqMbR
 
-// 20 - 22
-// https://decomp.me/scratch/T3LWL
-// https://decomp.me/scratch/3l0K5
-// https://decomp.me/scratch/7dyJm
+// 31 is latest
+// Worth checking all for notable comments when this is implemented
 
-// 22 is the most updated
-
-// The following levels will have no cases:
-// 16, 18, 28, 38, 40, 48
-
-// The following levels appear to be swim only
-// 10, 14, 15, 25, 35, 45
-
-// The following levels appear to be burn only
-// 12, 17, 27, 47
-
-// Water and skate
-// 11, 32
-
-// Bentley? and water (TODO)
-// 24, 41
-
-// All the same (TODO)
-// 30, 43, 44
-
-// Therefore the following levels are left to do:
-//          23 24    26
-// 30 31    33 34    36 37
-//    41 42 43 44    46
+// The following levels are left to do:
+//          33 34    36 37
+//       42          46
 // 50
 
 
 
-// Does not include 16 for some reason
+// Does not include 16
 #if (LEVEL_ID == 10) || (LEVEL_ID == 11) || (LEVEL_ID == 14) || (LEVEL_ID == 15) \
  || (LEVEL_ID == 20) || (LEVEL_ID == 21) || (LEVEL_ID == 23) || (LEVEL_ID == 24) \
  || (LEVEL_ID == 25) || (LEVEL_ID == 30) || (LEVEL_ID == 32) || (LEVEL_ID == 35) \
@@ -71,10 +56,9 @@
     #define IS_SKATEBOARDING_LEVEL
 #endif
 
-// Assuming level 26, but this may be different in practice
-#if (LEVEL_ID == 12) || (LEVEL_ID == 13) || (LEVEL_ID == 17) || (LEVEL_ID == 26) \
- || (LEVEL_ID == 27) || (LEVEL_ID == 30) || (LEVEL_ID == 33) || (LEVEL_ID == 37) \
- || (LEVEL_ID == 42) || (LEVEL_ID == 47)
+// Does not include 26 or 30
+#if (LEVEL_ID == 12) || (LEVEL_ID == 13) || (LEVEL_ID == 17) || (LEVEL_ID == 27) \
+ || (LEVEL_ID == 33) || (LEVEL_ID == 37) || (LEVEL_ID == 42) || (LEVEL_ID == 47)
     #define IS_FIRE_LAVA_LEVEL
 #endif
 
@@ -82,22 +66,29 @@
     #define IS_ICE_LEVEL
 #endif
 
-// Assuming it's a wooden ladder thing specifically
-#if (LEVEL_ID == 20) || (LEVEL_ID == 23)
-    #define IS_WOODEN_LADDER_LEVEL
-#endif
-
-// This isn't known yet
-#if (LEVEL_ID == 20)
-    #define IS_UNKNOWN_ANIM_33_LEVEL
+// To confirm: 33, 34, 42, 43, 44
+#if (LEVEL_ID == 20) || (LEVEL_ID == 23) || (LEVEL_ID == 30)
+    #define IS_LADDER_LEVEL
 #endif
 
 #if (LEVEL_ID == 13) || (LEVEL_ID == 22) || (LEVEL_ID == 26) || (LEVEL_ID == 34)
     #define IS_SGT_BYRD_LEVEL
 #endif
 
+// 31 is not included, boxing yeti is considered separate
+// and also doesn't have any special sound effects
+#if (LEVEL_ID == 24) || (LEVEL_ID == 36) || (LEVEL_ID == 41)
+    #define IS_BENTLEY_LEVEL
+#endif
+
+// Oops! They included 30?
+#if (LEVEL_ID == 30) || (LEVEL_ID == 33) || (LEVEL_ID == 43) || (LEVEL_ID == 44) \
+ || (LEVEL_ID == 46)
+    #define IS_AGENT_9_LEVEL
+#endif
+
 // WIP - taken from decomp.me on August 19th 2026
-void NAME_OVERLAY_FUNCTION(UnnamedOverlayFunction_Animation3) (void) {
+void NAME_OVERLAY_FUNCTION(PlaySpecialAnimationSounds) (void) {
     int animationId;
     int animationFrame;
 
@@ -175,7 +166,7 @@ void NAME_OVERLAY_FUNCTION(UnnamedOverlayFunction_Animation3) (void) {
         break;
     #endif
 
-    #ifdef IS_WOODEN_LADDER_LEVEL
+    #ifdef IS_LADDER_LEVEL
     case ANIMATION_STATE_LADDER_CLIMB:
         {
             int handler;
@@ -190,9 +181,6 @@ void NAME_OVERLAY_FUNCTION(UnnamedOverlayFunction_Animation3) (void) {
             }
             break;
         }
-    #endif
-
-    #ifdef IS_UNKNOWN_ANIM_33_LEVEL
     case ANIMATION_STATE_UNK_33:
     case ANIMATION_STATE_UNK_34:
         {
@@ -218,18 +206,53 @@ void NAME_OVERLAY_FUNCTION(UnnamedOverlayFunction_Animation3) (void) {
         break;
     #endif
 
+    #ifdef IS_BENTLEY_LEVEL
+    case 0x56:
+        {
+            int handler;
+            if (animationFrame == 2 || animationFrame == 11) {
+                handler = func_8003BABC(spyro.critterMobyPtr, 1, 0);
+                spyro.unk22[6] = animationFrame;
+                if (handler >= 0 && animationFrame == 11) {
+                    func_8003C0B0(handler, 0x1200);
+                }
+            }
+            break;
+        }
+    #endif    
+
     #ifdef IS_SGT_BYRD_LEVEL
     case 0x65:
-        if (animationFrame == 2 || animationFrame == 7) {
-            animationId = func_8003BABC(spyro.critterMobyPtr, 0, 0);
-            spyro.unk22[6] = animationFrame;
-            if (animationId >= 0 && animationFrame == 7) {
-                func_8003C0B0(animationId, 0x1200);
+        {
+            int handler;
+            if (animationFrame == 2 || animationFrame == 7) {
+                handler = func_8003BABC(spyro.critterMobyPtr, 0, 0);
+                spyro.unk22[6] = animationFrame;
+                if (handler >= 0 && animationFrame == 7) {
+                    func_8003C0B0(handler, 0x1200);
+                }
             }
+            break;
         }
-        break;
     #endif
-    
+
+    #ifdef IS_AGENT_9_LEVEL
+    case 0x74:
+        {
+            int handler;
+            if (animationFrame == 10 || animationFrame == 19) {
+                handler = func_8003BABC(spyro.critterMobyPtr, 0, 0);
+                spyro.unk22[6] = animationFrame;
+                if (handler >= 0) {
+                    if (animationFrame == 19) {
+                        func_8003C0B0(handler, 0x1200);
+                    }
+                }
+            }
+            break;
+        }
+    #endif
+        
     }
 }
 
